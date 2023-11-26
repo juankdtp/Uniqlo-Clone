d721823938cd48b6e8c6c8084f9bc144a14f48de
unknown <juankhosydtps@gmail.com>
2023-11-26 21:24:38 +0700

    Server Code Commit

--- server/orchestrator-express/controllers/controller.js
+++ server/orchestrator-express/controllers/controller.js
@@ -6,7 +6,7 @@ const USER_SERVICE_URL =
 const Redis = require("ioredis");
 const redis = new Redis({
   port: 16864, // Redis port
-  host: "redis-16864.c292.ap-southeast-1-1.ec2.cloud.redislabs.com", // Redis host
+  host: "", // Redis host
   username: "default", // needs Redis >= 6
   password: process.env.REDIS_PASS,
 });
